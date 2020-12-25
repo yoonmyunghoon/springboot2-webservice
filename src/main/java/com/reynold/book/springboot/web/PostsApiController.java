eea9755318e577d8936e6d19e339a72e23497db5
yoonmyunghoon <youn1791472@gmail.com>
2020-12-25 15:54:04 +0900

    test: Update PostsApiControllerTest
    feat: Update application.properties for h2 web console

--- src/main/java/com/reynold/book/springboot/web/PostsApiController.java
+++ src/main/java/com/reynold/book/springboot/web/PostsApiController.java
@@ -15,16 +15,19 @@ public class PostsApiController {
 
     @PostMapping("/api/v1/posts")
     public Long save(@RequestBody PostsSaveRequestDto requestDto) {
+
         return postsService.save(requestDto);
     }
 
     @PutMapping("/api/v1/posts/{id}")
     public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+
         return postsService.update(id, requestDto);
     }
 
     @GetMapping("api/v1/posts/{id}")
     public PostsResponseDto findById(@PathVariable Long id) {
+
         return postsService.findById(id);
     }
 
