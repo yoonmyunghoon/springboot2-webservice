7846597e8bdf430690445f08d0e632267e7690a6
yoonmyunghoon <youn1791472@gmail.com>
2020-12-31 17:31:34 +0900

    refactor: Update index methode in IndexController for using @LoginUser annotation
    Add LoginUser, LoginUserArgumentResolver, WebConfig for @LoginUser annotation

--- src/main/java/com/reynold/book/springboot/web/IndexController.java
+++ src/main/java/com/reynold/book/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.reynold.book.springboot.web;
 
+import com.reynold.book.springboot.config.auth.LoginUser;
 import com.reynold.book.springboot.config.auth.dto.SessionUser;
 import com.reynold.book.springboot.service.posts.PostsService;
 import com.reynold.book.springboot.web.dto.PostsResponseDto;
@@ -19,9 +20,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
